fa5adee1e7e74a30bdda98cb01dc9ccbce202299
Alex Benoit <Alex@Alexs-MacBook-Pro.local>
2014-11-09 22:35:42 -0800

    first rev of Laika project

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let stringFromTextField = humanYearsTextField.text.toInt()!
+        
+        dogYearsLabel.hidden = false
+        
+        dogYearsLabel.text = "\(stringFromTextField / 7)" + " dog years"
+        
+        humanYearsTextField.resignFirstResponder()
+    }
 
 }
 
